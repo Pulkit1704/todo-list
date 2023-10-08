26004be6160047a18f5c792e41f2b0cd0ca62c61
Pulkit <pgoel3379@gmail.com>
2023-10-08 13:03:21 +0530

    fix: splitting the input using whitespaces

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn runprompt(todo: &mut Vec<Task>){
 
         // take the args into the run function of lib and get the result of the computation out. 
         // * what if the user enter a single command without any white spaces. 
-        let args: Vec<&str> = buffer.split(" ").collect(); 
+        let args: Vec<&str> = buffer.split_whitespace().collect(); 
 
         todo_list::run(args, todo); 
         
